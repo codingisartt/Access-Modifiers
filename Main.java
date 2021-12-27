20f53a1afb27d1acb3a3bae39ed10b95c00cf6f2
Tubanur Balci <tubanurbalci@gmail.com>
2021-12-27 22:53:17 +0300

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        User u1=new User("Tubanur Balci");
+        User u2=new User("Patika Dev");
+
+        System.out.println(User.counter); 
+    }
+}
